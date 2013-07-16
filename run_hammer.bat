2196433a0a55fb6a46808a1af6ee9bbd5ec5011b
TheCallSign <TheCallSign@gmail.com>
2013-07-16 11:00:18 -0400

    Auto generated mod files by Source SDK

---
+++ run_hammer.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\da_singe\sourcesdk\bin\source2007\bin\hammer.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
---
+++ run_hlmv.bat
@@ -0,0 +1,2 @@
+@"c:\program files\steam\steamapps\da_singe\sourcesdk\bin\source2007\bin\hlmv.exe" -game "c:\program files\steam\steamapps\SourceMods\cliftonsource" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
